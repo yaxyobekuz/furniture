752eebd0ac7689bf96c79b60409f46dd0c574be7
yaxyobekb <pubgn9642@gmail.com>
2025-03-05 10:27:39 +0500

    Refactor Tailwind CSS configuration: streamline color and utility definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,92 +3,23 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        br:{
-          'primary':"#054C73",
-          'text-dark-gray':"#333333",
-          'dark':"#111111",
-          'light-gray':"#666666",
-        }
+      colors: {
+        br: {
+          dark: "#111111",
+          gray: "#333333",
+          primary: "#054C73",
+          "light-gray": "#666666",
+        },
       },
-      backgroundColor:{
-        'primary-dark':"#054C73",
-        'light-blue':"#F2F5FF",
-        'hero-light-blue':"#DFE9F4",
-        'primary':"#054C73",
-        'hero-light-gray':"#f1f1f1"
+      backgroundColor: {
+        "primary-dark": "#054C73",
+        "light-blue": "#F2F5FF",
+        "hero-light-blue": "#DFE9F4",
+        primary: "#054C73",
+        "hero-light-gray": "#f1f1f1",
       },
-      fontFamily:{
-        'montserrat':['Montserrat', 'sans-serif']
-      },
-      maxWidth:{
-        '1270':"1270px",
-        '1196':"1196px",
-        '1183':"1183px",
-        '1145':"1145px",
-        '514':"514px",
-        '486':"486px",
-        '400':"400px"
-      },
-      width:{
-        '568':"568px",
-        '500':"500px",
-        '381':"381px",
-        '372':"372px",
-        '355':"355px",
-        '229':"229px",
-        '90':"90px",
-        '60':"60px",
-        '84':"84px"
-      },
-      height:{
-        '90':"90px"
-      },
-      inset:{
-        'calc-50-45':"calc(50% - 45px)",
-        '45':"-45px",
-      },
-      padding:{
-        '197':"197px",
-        '155':"155px",
-        '150':"150px",
-        '133':"133px",
-        '90':"90px",
-        '46':"46px",
-        '60':"60px"
-      },
-      borderRadius:{
-        '50':"50px"
-      },
-      borderColor:{
-        'primary-dark':"#054C73"
-      },
-      borderWidth:{
-        '15':"15px",
-        '1':"1px"
-      },
-      fontSize:{
-        '34':"34px",
-        '52':"52px",
-        '32':"32px",
-      },
-      lineHeight:{
-        '41':"41.45px",
-        '39':"39px",
-        '65':"65px",
-        '35':"35px",
-        '29':"29.26px",
-        '24':"24.38px",
-        '17':"17px",
-        '19':"19px"
-      },
-      margin:{
-        '72':"72px",
-        '67':"67px",
-        '60':"60px",
-        '10':"10px"
-      }
+      fontFamily: { montserrat: ["Montserrat", "sans-serif"] },
     },
   },
   plugins: [],
-}
+};
